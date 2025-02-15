3bb72fd0f16127b963176882f190005af1d384de
Prathamesh Patil <22prathameshpatil.com>
2025-02-15 13:56:14 +0530

    fixed admin

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,13 +3,4 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'dist', // Ensure the correct output folder
-  },
-  server: {
-    historyApiFallback: true, // Enables proper routing
-  },
-  preview: {
-    historyApiFallback: true, // Ensures routing works in production
-  },
 });
