92db52967304f6e37f10c575f1389399f84d9cd4
Prathamesh Patil <143886933+22prathameshpatil@users.noreply.github.com>
2025-02-15 09:50:48 +0530

    Change for frontend url for handling cors error

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,7 @@ const cors = require("cors");
 const handleCheckAdmin = require("./middlewares/admin-middleware");
 
 const corsOptions = {
-  origin: "http://localhost:5173",
+  origin: "https://itconnect-frontend.onrender.com",
   methods:"GET,POST,PUT,DELETE,PATCH,HEAD",
   credentials: true,
 };
